aea69f7f52b4c6110d2b14f750d561bb757d400b
Carlos Herrera Conejero <carherco@gmail.com>
2018-03-14 09:27:16 +0100

    ejemplo para desregsitrar promesas

---
+++ sw_unregister.js
@@ -0,0 +1,8 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations()
+  .then(function(registrations) {
+    for(let registration of registrations) {
+      registration.unregister()
+    } 
+  })
+} 
